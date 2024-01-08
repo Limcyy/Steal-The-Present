e72af6a2b747bd9644169a34ea1b5ad3acf06d16
Adam Rana <adamek.rana@gmail.com>
2024-01-08 22:12:28 +0100

    refactor

--- code.js
+++ code.js
@@ -1,253 +1,214 @@
-var box = document.querySelector("#box");
-var rightButton = document.querySelector("#right")
-var box2 = document.querySelector("#box2");
-var boxx = document.querySelector("#boxX")
-var bo = document.querySelector("#bo")
-var ball = document.querySelector("#ball")
-var boxxx = document.querySelector("#boxxx")
-var retryButton = document.querySelector("#retryButton")
-var text = document.querySelector("#text")
+const player = document.querySelector("#player");
+const slowElf = document.querySelector("#slowElf");
+const fastElf = document.querySelector("#fastElf")
+const gift = document.querySelector("#gift")
+const snowBall = document.querySelector("#ball")
+const shootingElf = document.querySelector("#shootingElf")
+const retryButton = document.querySelector("#retryButton")
+const maxHeight = window.innerHeight - player.offsetWidth;
+const maxWidth = window.innerWidth - player.offsetWidth;
+
 retryButton.style.display = 'none';
-text.style.display = "none"
-let up2 = 1;
-let right2 = 1;
-let left2 = 1;
-let down2 = 1;
+
 retryButton.addEventListener("click", restartPage)
+
 function restartPage() {
-    // Reload the current page
-    location.reload();
-  }
+  location.reload();
+}
 
 
 function handleKeyPressD(event) {
-    if (event.key === 'D' || event.key === 'd') {
-      right()
-      console.log(window.innerHeight, window.innerWidth )
-    }
+  if (event.key === 'D' || event.key === 'd') {
+    right()
   }
-  function handleKeyPressW(event) {
-    if (event.key === 'W' || event.key === 'w') {
-     up()
-    }
-  }
-  function handleKeyPressS(event) {
-    if (event.key === 'S' || event.key === 's') {
-      down()
-    }
-  }
-  function handleKeyPressA(event) {
-    if (event.key === 'A' || event.key === 'a') {
-      left()
-    }
-  }
-
-  document.addEventListener('keydown', handleKeyPressD);
-  document.addEventListener('keydown', handleKeyPressW);
-  document.addEventListener('keydown', handleKeyPressS);
-  document.addEventListener('keydown', handleKeyPressA);
-
-let position = 0
-let direction = 1;
-const maxWidth = window.innerHeight - box.offsetWidth;
-const xmaxWidth = window.innerWidth - box.offsetWidth;
-righ()
-const xfirstImage = 'url(src/pixil-frame-0-7.png)';
-    const xsecondImage = 'url(src/pixil-frame-0-6.png)';
-    let xcurrentImage = xfirstImage;
-function righ() {
-    box.style.left = 30 * 20 + "px"
-    
-    
-        position += 4 * direction;
-        if (position > maxWidth || position === 0) {
-            direction *= -1;
-            xcurrentImage = xcurrentImage === xfirstImage ? xsecondImage : xfirstImage;
-      box.style.backgroundImage = xcurrentImage;
-          }
-          box.style.bottom = position + 'px'
-};
-    let xposition = 0
-    let xdirection = 1
-    const firstImage = 'url(src/pixil-frame-0-7.png)';
-    const secondImage = 'url(src/pixil-frame-0-6.png)';
-    let currentImage = firstImage;
-    function rig() {
-    boxx.style.left = 30 * 14 + "px"
-    
-    
-        xposition += 4 * xdirection;
-        if (xposition > maxWidth || xposition === 0) {
-            xdirection *= -1;
-            currentImage = currentImage === firstImage ? secondImage : firstImage;
-      boxx.style.backgroundImage = currentImage;
-          }
-        boxx.style.bottom = xposition + 'px'
-        
-        
-    
-  };
-  function ri() {
-    bo.style.right = 10 + "px"
-    bo.style.bottom = 10 + "px"
-     
-  };
-  let xxdirection = 1
-    let xxposition = 0
-  function r() {
-    ball.style.right = 90 + "px"
-    ball.style.bottom = "510px"
-    xxposition += 6 * xxdirection;
-    
-    
-    if (xxposition > xmaxWidth + 90 || xxposition === 0) {
-        xxposition = 90
-        
-       }
-       ball.style.right = xxposition + 'px'
-  }
-setInterval(r, 3)
-setInterval(ri, 10)
-setInterval(rig, 10)
-setInterval(righ, 5)
-
-box2.style.top = "0px"
-box2.style.left = "0px"
-
-
-let xxwinCount = localStorage.getItem("xxwincount")
-function save() {
-  xxwinCount++
-  localStorage.setItem("xxwincount", xxwinCount);
-  console.log(localStorage.getItem("xxwincount"))
-  
-
 }
+function handleKeyPressW(event) {
+  if (event.key === 'W' || event.key === 'w') {
+    up()
+  }
+}
+function handleKeyPressS(event) {
+  if (event.key === 'S' || event.key === 's') {
+    down()
+  }
+}
+function handleKeyPressA(event) {
+  if (event.key === 'A' || event.key === 'a') {
+    left()
+  }
+}
+
+document.addEventListener('keydown', handleKeyPressD);
+document.addEventListener('keydown', handleKeyPressW);
+document.addEventListener('keydown', handleKeyPressS);
+document.addEventListener('keydown', handleKeyPressA);
+
+player.style.top = "0px"
+player.style.left = "0px"
+
+
 function up() {
-    
-    box2.style.top = (parseInt(box2.style.top) - 30) + 'px';
-    
+  player.style.top = (parseInt(player.style.top) - 30) + 'px';
 }
-  
-  function right() {
-    box2.style.left = (parseInt(box2.style.left) + 30) + 'px';
-  }
-  
-  function left() {
-    box2.style.left = (parseInt(box2.style.left) - 30) + 'px';
-  }
-  
-  function down() {
-    box2.style.top = (parseInt(box2.style.top) + 30) + 'px';
-  }
-  var x = setInterval((function checkCollision() {
-    
-    const rect1 = box.getBoundingClientRect();
-    const rect2 = box2.getBoundingClientRect();
-    const rect3 = boxx.getBoundingClientRect();
-    const rect4 = bo.getBoundingClientRect();
-    const rect5 = ball.getBoundingClientRect()
-    if (
-      rect1.right >= rect2.left &&
-      rect1.left <= rect2.right &&
-      rect1.bottom >= rect2.top &&
-      rect1.top <= rect2.bottom 
-      
-    ) {
-        box2.style.display = 'none';
-        boxx.style.display = 'none';
-        ball.style.display = 'none';
-        bo.style.display = 'none';
-        boxxx.style.display = 'none';
-        box.style.display = 'none';
-        document.body.style.backgroundImage = "url(src/pixil-frame-0-12.png)"
-        retryButton.style.display = "block"
-        clearInterval(x)
-    }
-    if (
-      rect3.right >= rect2.left &&
-      rect3.left <= rect2.right &&
-      rect3.bottom >= rect2.top &&
-      rect3.top <= rect2.bottom
-    ) {
-        box2.style.display = 'none';
-        boxx.style.display = 'none';
-        ball.style.display = 'none';
-        bo.style.display = 'none';
-        boxxx.style.display = 'none';
-        box.style.display = 'none';
-        document.body.style.backgroundImage = "url(src/pixil-frame-0-12.png)"
-        retryButton.style.display = "block"
-        clearInterval(x)
-        console.log(rect2.left)
-    }
 
-     if (
-      rect4.right >= rect2.left &&
-      rect4.left <= rect2.right &&
-      rect4.bottom >= rect2.top &&
-      rect4.top <= rect2.bottom
-     )
-     
-     {
-        box2.style.display = 'none';
-        boxx.style.display = 'none';
-        ball.style.display = 'none';
-        bo.style.display = 'none';
-        boxxx.style.display = 'none';
-        box.style.display = 'none';
-        document.body.style.backgroundImage = "url(src/pixil-frame-0-13.png)"
-        retryButton.style.backgroundImage = "url(src/pixil-frame-0-17.png)"
-        retryButton.style.display = "block"
-        text.style.display = "block"
-        save()
-        text.innerHTML = "U won " + localStorage.getItem("xxwincount") + " times"
-        clearInterval(x)
-    }
-     if (
-        rect5.right >= rect2.left &&
-        rect5.left <= rect2.right &&
-        rect5.bottom >= rect2.top &&
-        rect5.top <= rect2.bottom
-       )
-       
-       {
-        box2.style.display = 'none';
-        boxx.style.display = 'none';
-        ball.style.display = 'none';
-        bo.style.display = 'none';
-        boxxx.style.display = 'none';
-        box.style.display = 'none';
-        document.body.style.backgroundImage = "url(src/pixil-frame-0-12.png)"
-        retryButton.style.display = "block"
-        
-        
-        
-        clearInterval(x)
-    
-      } 
-    if  (
-        0 > rect2.left ||
-        window.innerWidth < rect2.right ||
-        window.innerHeight < rect2.bottom ||
-        0 > rect2.top
-        )
-        {
-            box2.style.display = 'none';
-            boxx.style.display = 'none';
-            ball.style.display = 'none';
-            bo.style.display = 'none';
-            boxxx.style.display = 'none';
-            box.style.display = 'none';
-            document.body.style.backgroundImage = "url(src/pixil-frame-0-12.png)"
-            retryButton.style.display = "block"
-            clearInterval(x)
-        } 
-  }), 10)
-  
-  // Check collision on an interval (e.g., every 100 milliseconds)
-  
+function right() {
+  player.style.left = (parseInt(player.style.left) + 30) + 'px';
+}
 
-//if (left2 === 1000) {
-        //    clearInterval(x);
-       // }
+function left() {
+  player.style.left = (parseInt(player.style.left) - 30) + 'px';
+}
+
+function down() {
+  player.style.top = (parseInt(player.style.top) + 30) + 'px';
+}
+
+let slowElfPosition = 0
+let slowElfDirection = 1;
+
+
+const firstImage = 'url(src/pixil-frame-0-7.png)';
+const secondImage = 'url(src/pixil-frame-0-6.png)';
+
+let slowElfCurrentImage = firstImage;
+
+slowElf.style.left = 30 * 20 + "px"
+function slowElfMoving() {
+
+  slowElfPosition += 4 * slowElfDirection;
+
+  if (slowElfPosition > maxHeight || slowElfPosition === 0) {
+    slowElfDirection *= -1;
+    slowElfCurrentImage = slowElfCurrentImage === firstImage ? secondImage : firstImage;
+    slowElf.style.backgroundImage = slowElfCurrentImage;
+  }
+
+  slowElf.style.bottom = slowElfPosition + 'px'
+
+};
+
+let fastElfPosition = 0
+let fastElfDirection = 1
+let fastElfCurrentImage = firstImage;
+
+fastElf.style.left = 30 * 14 + "px"
+
+function fastElfMoving() {
+
+  fastElfPosition += 20 * fastElfDirection;
+
+  if (fastElfPosition > maxHeight || fastElfPosition === 0) {
+    fastElfDirection *= -1;
+    fastElfCurrentImage = fastElfCurrentImage === firstImage ? secondImage : firstImage;
+    fastElf.style.backgroundImage = fastElfCurrentImage;
+  }
+
+  fastElf.style.bottom = fastElfPosition + 'px'
+
+};
+
+  gift.style.right = 10 + "px"
+  gift.style.bottom = 10 + "px"
+
+
+let snowBallDirection = 1
+let snowBallPosition = 0
+
+snowBall.style.right = 90 + "px"
+  snowBall.style.bottom = "510px"
+
+function snowBallMoving() {
+  
+  snowBallPosition += 6 * snowBallDirection;
+
+  if (snowBallPosition > maxWidth + 90 || snowBallPosition === 0) {
+    snowBallPosition = 90
+
+  }
+
+  snowBall.style.right = snowBallPosition + 'px'
+}
+
+
+setInterval(slowElfMoving, 10)
+setInterval(snowBallMoving, 4)
+setInterval(fastElfMoving, 5)
+
+function death() {
+    slowElf.style.display = 'none';
+    player.style.display = 'none';
+    fastElf.style.display = 'none';
+    snowBall.style.display = 'none';
+    gift.style.display = 'none';
+    shootingElf.style.display = 'none';
+    player.style.display = 'none';
+    document.body.style.backgroundImage = "url(src/pixil-frame-0-12.png)"
+    retryButton.style.display = "block"
+    clearInterval(DiedOrWinInterval)
+}
+ 
+function win() {
+    slowElf.style.display = 'none';
+    fastElf.style.display = 'none';
+    snowBall.style.display = 'none';
+    gift.style.display = 'none';
+    shootingElf.style.display = 'none';
+    player.style.display = 'none';
+    document.body.style.backgroundImage = "url(src/pixil-frame-0-13.png)"
+    retryButton.style.backgroundImage = "url(src/pixil-frame-0-17.png)"
+    retryButton.style.display = "block"
+    clearInterval(DiedOrWinInterval)
+}
+ 
+const DiedOrWinInterval = setInterval((function checkCollision() {
+
+  const slowElfsBorder = slowElf.getBoundingClientRect();
+  const playersBorder = player.getBoundingClientRect();
+  const rect3 = fastElf.getBoundingClientRect();
+  const rect4 = gift.getBoundingClientRect();
+  const rect5 = snowBall.getBoundingClientRect()
+  
+  if (
+    slowElfsBorder.right >= playersBorder.left &&
+    slowElfsBorder.left <= playersBorder.right &&
+    slowElfsBorder.bottom >= playersBorder.top &&
+    slowElfsBorder.top <= playersBorder.bottom
+
+  ) {
+    death()
+  }
+  if (
+    rect3.right >= playersBorder.left &&
+    rect3.left <= playersBorder.right &&
+    rect3.bottom >= playersBorder.top &&
+    rect3.top <= playersBorder.bottom
+  ) {
+    death()
+
+  }
+
+  if (
+    rect4.right >= playersBorder.left &&
+    rect4.left <= playersBorder.right &&
+    rect4.bottom >= playersBorder.top &&
+    rect4.top <= playersBorder.bottom
+  ) {
+   win()
+  }
+  if (
+    rect5.right >= playersBorder.left &&
+    rect5.left <= playersBorder.right &&
+    rect5.bottom >= playersBorder.top &&
+    rect5.top <= playersBorder.bottom
+  ) {
+    death()
+  }
+  if (
+    0 > playersBorder.left ||
+    window.innerWidth < playersBorder.right ||
+    window.innerHeight < playersBorder.bottom ||
+    0 > playersBorder.top
+  ) {
+    death()
+  }
+}), 10)
